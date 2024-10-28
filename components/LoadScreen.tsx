b5cbc78021e089595fc33afd2f5d59ae0a632ccf
Pelumi Adejumo <adejumopelumi63@gmail.com>
2024-10-28 20:15:51 +0100

    Create Account page in progress

--- components/LoadScreen.tsx
+++ components/LoadScreen.tsx
@@ -39,16 +39,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "white",
-    alignItems: "center"
+    alignItems: "center",
+    width: "100%"
 },
   text: {
-       fontSize: 30
+       fontSize: 30,
+       fontWeight: "bold"
    },
    background: {
-    flex: 1,
     width: '100%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: "center"
    },
 })
