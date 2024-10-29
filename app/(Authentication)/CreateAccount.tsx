403ae725a89db272022d27c72dd228eb9d5477fb
oyindamola adeonigbagbe <oyindamolatabitha@gmail.com>
2024-10-29 12:51:29 +0100

    test

--- app/(Authentication)/CreateAccount.tsx
+++ app/(Authentication)/CreateAccount.tsx
@@ -1,105 +1,34 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import React, {useState} from 'react';
-import { Link, router } from 'expo-router'
-import Input from '@/components/Input';
-
-const { width, height } = Dimensions.get("window");
-const isTablet = width >= 768;
-
-
-type Errors = {
-  firstname: string | null;
-  email: string | null;
-  password: string | null;
-}
-
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function CreateAccount() {
-
-   
-  const [firstname, setfirstname] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [error, setErrors] = useState<Errors>({
-    firstname : null,
-    email : null,
-    password: null
-  })
-
-
-
-  const isValidEmail = (email: string): string | null => {
-    if (!email.includes('@')) {
-      return 'Please enter a valid email';
-    }
-    return null;
-  };
-
-   
-  const handleContinue = () => {
-    
-    const newErrors: {
-      firstname: string | null;
-      email: string | null;
-      password: string | null;
-    } = {
-      firstname: firstname.trim() === '' ? 'First name is required' : null,
-      email: email.trim() === '' ? 'Email is required' : isValidEmail(email),
-      password: password.trim() === '' ? 'Phone number is required' : null,
-  
-    };
-  
-    setErrors(newErrors);
-  
-    if (!Object.values(newErrors).some((error) => error !== null)) {
-      router.navigate("./");
-    }
-  };
-
 
   return (
     <View style={styles.container}>
       <View style={styles.subcontainer}>
-        <Text style={styles.topText}>Create Account</Text>
+          <Text style={styles.topText}>Create Account</Text>
 
-        <View style={{width: "100%", bottom: "3.6%", left: "3%"}}>
-           <Input value={firstname} setValue={setfirstname} placeholder='First Name' error={error.firstname} />
-        </View>
-
-        <View style={{width: "100%", bottom: "3.6%", left: "3%"}}>
-           <Input value={email} setValue={setEmail} placeholder='E-mail' error={error.email} />
-        </View>
-
-        <View style={{width: "100%", bottom: "3.6%", left: "3%"}}>
-           <Input value={password} setValue={setPassword} placeholder='Password' error={error.password} />
-        </View>
       </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "100%",
-  },
-  subcontainer: {
-    backgroundColor: "white",
-    width: "100%",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    padding: isTablet ? 30 : 20,
-   // Increased padding for tablets
-     // Center content horizontally
-  },
-  topText: {
-    fontSize: isTablet ? 50 : 40, // Adjusted font size for tablets
-    color: "blue",
-    fontWeight: "500",
-    textAlign: "left"
-  },
-});
-
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "blue",
+        alignItems: "center"
+    },
+    text: {
+        fontSize: 30,
+    },
+    subcontainer: {
+       borderTopStartRadius: 40,
+    },
+    topText: {
+        fontSize: 40,
+        color: "blue",
+    },
+    
+})
