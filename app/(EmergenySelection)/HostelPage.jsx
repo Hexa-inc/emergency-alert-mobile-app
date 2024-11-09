134ad26a724de98c82c1fcc7e03e63cba5c2b184
oyindamola adeonigbagbe <oyindamolatabitha@gmail.com>
2024-11-09 13:41:51 +0100

    oyin's task completed

--- app/(EmergenySelection)/HostelPage.jsx
+++ app/(EmergenySelection)/HostelPage.jsx
@@ -1,11 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 
 const HostelPage = () => {
   return (
-    <View style={styles.container}>
-      <Text>HostelPage</Text>
+    <View style={styles.container}>    
+      <AntDesign name="arrowleft" size={30} color="black" style={{marginTop:29, marginLeft:10}}/>
+      <View style={{backgroundColor:"white",marginTop:37,borderTopLeftRadius:35, borderTopRightRadius:35, 
+        borderBottomLeftRadius:15,borderBottomRightRadius:15, padding:10,}}>
+      <Text style={{fontSize:25, fontWeight:"bold", marginVertical:20}}>Select Hostel</Text>
+
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>New Elizabeth Hall</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Elizabeth 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}> Elizabeth 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Elizabeth 3</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black",  marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>Guest House</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Daniel 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black",  marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>House of Daniel</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:15}}>
+        <Text style={{fontSize:20,padding:15}}>Regional House</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:30}}>
+        <Text style={{fontSize:20,padding:15}}>Chapel</Text>
+      </TouchableOpacity>
+      </View>
+     
     </View>
+   
   )
 }
 
@@ -14,11 +51,7 @@ export default HostelPage
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignContent: "center",
-        alignSelf: "center",
-        width: "100%"
+        backgroundColor: 'blue',
+     
       },
 })
--- app/(EmergenySelection)/SicknessPage.jsx
+++ app/(EmergenySelection)/SicknessPage.jsx
@@ -1,10 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const SicknessPage = () => {
   return (
     <View style={styles.container}>
-      <Text>SicknessPage</Text>
+      <ScrollView>
+    <AntDesign name="arrowleft" size={30} color="black" style={{marginTop:29, marginLeft:10}}/>
+      <View style={{backgroundColor:"white",marginTop:37,borderTopLeftRadius:35, borderTopRightRadius:35, 
+        borderBottomLeftRadius:15,borderBottomRightRadius:15, padding:10,}}>
+      <Text style={{fontSize:25, fontWeight:"bold", marginVertical:20}}>Sickness</Text>
+
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>Allergic Reaction</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Anxiety/Panic disorder</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>Bleeding</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Chestpain</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black",  marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>Difficulty Breathing</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black"}}>
+        <Text style={{fontSize:20,padding:15}}>Fainting</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black",  marginVertical:15}}>
+        <Text style={{fontSize:20,padding:15}}>Heat Stroke</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:15}}>
+        <Text style={{fontSize:20,padding:15}}>Menstrual Cramps</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:15}}>
+        <Text style={{fontSize:20,padding:15}}>Seizures</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:15}}>
+        <Text style={{fontSize:20,padding:15}}>Ulcer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:15}}>
+        <Text style={{fontSize:20,padding:15}}>Viral infection</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:20, borderWidth:1, borderColor:"black", marginBottom:60}}>
+        <Text style={{fontSize:20,padding:15}}>Poisonous intake</Text>
+      </TouchableOpacity>
+      </View>
+      </ScrollView>
     </View>
   )
 }
@@ -14,9 +58,7 @@ export default SicknessPage
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: "100%"
+        backgroundColor: 'blue',
+       
       },
 })
