8c8c185f804460a79d26e5d5a239b2c6de347e9c
oyindamola adeonigbagbe <oyindamolatabitha@gmail.com>
2024-11-12 16:37:09 +0100

    conf

--- app/(EmergencyAction)/ConfirmationPage.jsx
+++ app/(EmergencyAction)/ConfirmationPage.jsx
@@ -5,8 +5,7 @@ import Entypo from '@expo/vector-icons/Entypo';
 const ConfirmationPage = () => {
   return (
     <View style={styles.container}>
-      {/* <Text>ConfirmationPage</Text> */}
-      {/* <Image source={{uri: 'https://static.vecteezy.com/system/resources/thumbnails/017/350/123/small/green-check-mark-icon-in-round-shape-design-png.png'}} style={{}}/> */}
+    
       <View style={{backgroundColor:"green", borderRadius:100,}}>
       <Entypo name="check" size={45} color="white" style={{padding:10}}/>
       </View>
